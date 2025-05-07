d074cea6d057efa5499c21fe340dffdc8a6e5cc1
Afam <136574889+josh-fam@users.noreply.github.com>
2025-05-07 10:28:23 +0100

    Update 01_Cleaning.sql

--- queries/01_Cleaning.sql
+++ queries/01_Cleaning.sql
@@ -1,4 +1,18 @@
-
--- Step 2: Results from Cleaned View (removed duplicates)
-SELECT * FROM 
-    cleanedsales;
+-- Step 1: Create Cleaned View (remove nulls, bad data, and calculate TotalPrice)
+CREATE VIEW CleanedSales AS
+SELECT
+    InvoiceNo,
+    StockCode,
+    Description,
+    Quantity,
+    UnitPrice,
+    Quantity * UnitPrice AS TotalPrice,
+    InvoiceDate,
+    CustomerID,
+    Country
+FROM sales_data
+WHERE
+    CustomerID IS NOT NULL
+    AND Quantity > 0
+    AND UnitPrice > 0;
+SELECT * FROM CleanedSales LIMIT 10;
